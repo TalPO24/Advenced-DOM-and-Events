1f403ed6d83be88ad5faa8a4a11eca8d64963e76
Tal Poris <119438047+TalPO24@users.noreply.github.com>
2023-04-24 13:28:03 +0300

    Advanced DOM and Events

--- script.js
+++ script.js
@@ -28,3 +28,41 @@ document.addEventListener('keydown', function(e) {
         closeModal();
     }
 });
+
+//* Selecting, Creating, and Deleting Elements
+// Selecting elements
+console.log(document.documentElement)
+console.log(document.head)
+console.log(document.body)
+
+const header = document.querySelector('.header')
+const allSections = document.querySelectorAll('.section')
+console.log(allSections)
+
+document.getElementById('#section--1')
+const allButtons = document.getElementsByTagName('button')
+console.log(allButtons)
+
+console.log(document.getElementsByClassName('btn'))
+
+// Creating and inserting elements 
+const message = document.createElement('div')
+message.classList.add('cookie-message')
+message.textContent = 'we use cookies to improve functionallity and analytics.'
+message.innerHTML = 'we use cookies to improve functionallity and analytics. <button class="btn btn--close-cookie">Got it!</button>'
+
+// The Element.prepend() method inserts a set of Node objects or string objects before the first child of the Element. String objects are inserted as equivalent Text nodes.
+//  header.prepend(message)
+
+// The Element.append() method inserts a set of Node objects or string objects after the last child of the Element. String objects are inserted as equivalent Text nodes.
+header.append(message)
+
+// header.before(message)
+// header.after(message)
+
+// Delete elements
+document.querySelector('.btn--close-cookie').
+addEventListener('click', function() {
+    // message.remove() // The Element.remove() method removes the element from the DOM.
+    message.parentElement.removeChild(message)
+})
