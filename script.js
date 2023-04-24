59c5c6d493270ebee7322edfc7b991878cb7c555
Tal Poris <119438047+TalPO24@users.noreply.github.com>
2023-04-24 19:40:25 +0300

    Advanced DOM and Events

--- script.js
+++ script.js
@@ -66,3 +66,48 @@ addEventListener('click', function() {
     // message.remove() // The Element.remove() method removes the element from the DOM.
     message.parentElement.removeChild(message)
 })
+
+//* Styles Attributes and Classes
+// Styles
+message.style.backgroundColor = '#37383d'
+message.style.width = '120%'
+console.log(message.style.color)
+console.log(message.style.backgroundColor)
+
+console.log(getComputedStyle(message).color)
+console.log(getComputedStyle(message).height)
+
+message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) + 40 + 'px'
+
+document.documentElement.style.setProperty('--color-primary', 'orangered') // The CSSStyleDeclaration.setProperty() method interface sets a new value for a property on a CSS style declaration object.
+
+// Attributes
+const logo = document.querySelector('.nav__logo')
+console.log(logo.alt)
+console.log(logo.className)
+
+logo.alt = 'Beautiful minimalist logo'
+
+// Non-standard
+console.log(logo.designer)
+console.log(logo.getAttribute('designer'))
+logo.setAttribute('company', 'Bankist')
+
+console.log(logo.src)
+console.log(logo.getAttribute('src'))
+
+const link = document.querySelector('.twitter-link')
+console.log(link.href)
+console.log(link.getAttribute('href'))
+
+// Data attributes
+console.log(logo.dataset.versionNumber)
+
+// Classes
+logo.classList.add("c", "j")
+logo.classList.remove("c", "j")
+logo.classList.toggle("c")
+logo.classList.contains("c")
+
+//* Don't use 
+// logo.className = 'Jonas'
